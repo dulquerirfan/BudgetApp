a49e6f8ce5f812c390b09b65f6d2257307ae74f6
Bennur Irfan <irfansrg690@gmail.com>
2021-09-30 08:02:09 +0530

    debugged

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state,action)=>{
+const AppReducer= (state,action) => {
     switch(action.type){
         case 'ADD_TRANSACTION':
             return {
@@ -9,3 +9,4 @@ export default (state,action)=>{
             return state;
     }
 }
+export default AppReducer;
